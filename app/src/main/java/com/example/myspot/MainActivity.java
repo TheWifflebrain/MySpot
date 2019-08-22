763f1db7db5de1b144f5db1b5fe1a652ef228191
TheWifflebrain <blackice150@gmail.com>
2019-08-21 23:25:48 -0400

    Got initial struture done

--- app/src/main/java/com/example/myspot/MainActivity.java
+++ app/src/main/java/com/example/myspot/MainActivity.java
@@ -2,12 +2,46 @@ package com.example.myspot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button buttonCalculate;
+    public Button buttonReset;
+    public EditText setNumSpots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonCalculate = findViewById(R.id.calc);
+        buttonReset = findViewById(R.id.reset);
+        setNumSpots = findViewById(R.id.numSpots);
+
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+
+        });
+    }
+
+    public int josephusCalculations(int spots){
+        return spots;
     }
 }
