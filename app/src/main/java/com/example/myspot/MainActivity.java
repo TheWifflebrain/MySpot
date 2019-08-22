9716e5f8dc35384f199be69828db65c19043964f
TheWifflebrain <blackice150@gmail.com>
2019-08-22 12:46:36 -0400

    got the basics of buttonCalculate

--- app/src/main/java/com/example/myspot/MainActivity.java
+++ app/src/main/java/com/example/myspot/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     public Button buttonCalculate;
     public Button buttonReset;
     public EditText setNumSpots;
+    public TextView txtResults;
+    public String numSpots = "1";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,23 @@ public class MainActivity extends AppCompatActivity {
         buttonCalculate = findViewById(R.id.calc);
         buttonReset = findViewById(R.id.reset);
         setNumSpots = findViewById(R.id.numSpots);
+        txtResults = findViewById(R.id.results);
 
         buttonCalculate.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                numSpots = setNumSpots.getText().toString();
+
+                if(numSpots.equals(" ")){
+                    numSpots = "1";
+                }
+
+                int spots = Integer.parseInt(numSpots);
+                spots = josephusCalculations(spots);
+
+                numSpots = String.valueOf(spots);
+                txtResults.append(numSpots);
 
             }
 
